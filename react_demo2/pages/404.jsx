bd1561afa5a4f1e3c4d5491e452bc0377bbdef5e
Sergey Belski <s.belskyi@e-comexpert.ru>
2022-02-03 12:21:27 +0300

    404 страница начало

--- react_demo2/pages/404.jsx
+++ react_demo2/pages/404.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import Link from "next/link";
 
-function TestPage() {
+function Page404() {
     
     return (
         <div>
             <Link href="/">
                 <a>home</a>
             </Link>
-            <h1>Тестовая страница</h1>
+            <h1>Тестовая страница 404</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam alias nam earum dicta ipsam quasi eum eveniet perspiciatis, possimus placeat. Inventore itaque quaerat saepe, eveniet veritatis suscipit tempore doloribus ullam.</p>
         </div>
     )
 }
 
-export default TestPage;
+export default Page404;
